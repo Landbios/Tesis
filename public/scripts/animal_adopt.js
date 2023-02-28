2aa703fccd92789eee5a93def5a3d0bb8fe2866f
= <gonzru01atwork@gmail.com>
2023-02-28 16:25:47 -0400

    favorite animal function

--- public/scripts/animal_adopt.js
+++ public/scripts/animal_adopt.js
@@ -112,29 +112,37 @@ fetch(location.href, { method: 'POST' })
         while (cardContainer.childElementCount < data.length) {
             cardContainer.innerHTML += `
                     <div class="col-md-3">
+                        <div class="card">
+                            
+                            <img class="card-img-top"
+                                src="https://images.unsplash.com/photo-1589952283406-b53a7d1347e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                                alt="">
+                            <div class="card-body">
+                                
+                                    <button class="btn-heart">🤍</button>
+                                    <h4 class="animal-name">${data[cardContainer.childElementCount].nombre}</h4>
+                                
+                                
+                                <h5 class="animal-data">${utils.makeSpecieFull(data[cardContainer.childElementCount].especie)}</h5>
 
-                    <div class="card">
-                        
-                    <img class="card-img-top"
-                        src="https://images.unsplash.com/photo-1589952283406-b53a7d1347e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                        alt="">
-                    <div class="card-body">
-                        <h4 class="animal-name">${data[cardContainer.childElementCount].nombre}</h4>
-                        <h5 class="animal-data">${utils.makeSpecieFull(data[cardContainer.childElementCount].especie)}</h5>
-                        <h5 class="animal-data">${data[cardContainer.childElementCount].edad} <span>${data[cardContainer.childElementCount].tipo === 'a' ? 'años' : 'meses'}</span></h5>
-                        <div class="row">
-                        <div class="col-md-6">
-                            <a href=""  class="btn btn-adopt btn-lg btn-hero">${getUsernameFromSingleCookie(document.cookie) === data[cardContainer.childElementCount].propietario ? '' : 'Adoptar'}</a>
+                                <h5 class="animal-data">${data[cardContainer.childElementCount].edad} <span>${data[cardContainer.childElementCount].tipo === 'a' ? 'años' : 'meses'}</span></h5>
+
+                                <div class="row">
+
+                                    <div class="col-md-6">
+                                        <a href=""  class="btn btn-adopt btn-lg btn-hero">${getUsernameFromSingleCookie(document.cookie) === data[cardContainer.childElementCount].propietario ? '' : 'Adoptar'}</a>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <a href="/animal/${data[cardContainer.childElementCount].id}" class="btn btn-lg btn-hero">Ver mas</a>
+                                    </div>
+                                
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                            <a href="/animal/${data[cardContainer.childElementCount].id}" class="btn btn-lg btn-hero">Ver mas</a>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
             
                 </div>
-            `
+            `;
         }
 
         const btnAdopt = document.querySelectorAll('.btn-adopt');
